cb076d9bbe9da13c968db55099fd49d96a2491db
isabelpaiva <isxpaiva@icloud.com>
2023-06-20 13:45:04 -0300

    feat: Add models for accounts and news

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Criar uma classe que herde abstractUser
-#  add  no settings.py AUTH_USER_MODEL, tendo como valor o caminho ate a model de usuario
-# rodar migraçao
-
 class Account(AbstractUser):
-    ...
+    email = models.EmailField(unique=True)
+    is_text_reviewer = models.BooleanField()
+
--- news/models.py
+++ news/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class News(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    reviewers = models.ManyToManyField('accounts.Account', related_name = 'news', through="news.NewsReview")
+
+class NewsReview(models.Model):
+    reviewer = models.ForeignKey('accounts.Account', on_delete=models.CASCADE)
+    news = models.ForeignKey('news.News', on_delete=models.CASCADE)
+    feedback = models.TextField()
