3004ebf05906ee07f1bf095eb8421ba31cdd2eb0
isabelpaiva <isxpaiva@icloud.com>
2023-06-20 19:32:30 -0300

    feat(news): Implement serializers and views

--- news/serializers.py
+++ news/serializers.py
@@ -1,1 +1,25 @@
 from rest_framework import serializers
+from .models import News, NewsReview
+
+class NewsSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only= True)
+    title = serializers.CharField()
+    content = serializers.CharField()
+    created_at = serializers.DateTimeField(read_only=True)
+    updated_at = serializers.DateTimeField(read_only=True)
+
+    def create(self, validated_data):
+        return News.objects.create(**validated_data)
+    
+
+class NewsReviewSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    feedback = serializers.CharField()
+    reviewer_username = serializers.CharField(read_only=True, source="reviewer.username")
+    news_id = serializers.SerializerMethodField()
+
+    def get_news_id(self, value):
+        return value.news.id
+
+    def create(self, validated_data):
+        return NewsReview.objects.create(**validated_data)
--- news/views.py
+++ news/views.py
@@ -20,3 +20,11 @@ class CreateListNewView(APIView):
         return Response(serializer.data)
     
 
+class CreateListReview(APIView):
+    authentication_classes = [JWTAuthentication]
+    def post(self, request: Request, news_id: int):
+        serializer = NewsReviewSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        news = get_object_or_404(News, id=news_id)
+        serializer.save(news=news, reviewer=request.user)
+        return Response(serializer.data, status.HTTP_201_CREATED)
